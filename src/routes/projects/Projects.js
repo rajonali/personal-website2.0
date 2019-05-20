8d5d15a5a94d5faec80331a4bd34875291a27ea2
Rajon Ali <rajonali44492@icloud.com>
2019-05-20 14:35:37 -0500

    Fixed link

--- src/routes/projects/Projects.js
+++ src/routes/projects/Projects.js
@@ -18,7 +18,7 @@ class Projects extends Component {
     <p class="card-text text-dark">An app for a convenience store to organize their inventory and sales to create detailed reports with minimal UI experience and interaction.</p>
     <p class="card-text text-dark">Technologies: MongoDB, Express, React, and Node</p>
       </div>
-      <a href="#" style={{padding:'10px', borderRadius:'0px', backgroundColor:'#d9534f'}} class="btn btn-danger">Go to Github Repo</a>
+      <a href="https://github.com/rajonali/CRUD-Inventory-Mgmt-Tool" style={{padding:'10px', borderRadius:'0px', backgroundColor:'#d9534f'}} class="btn btn-danger">Go to Github Repo</a>
 
 </div>
 </div>
@@ -33,7 +33,7 @@ class Projects extends Component {
     <p class="card-text text-dark">A MERN app to find and curate a database of establishments that take Apple Pay and also allow users to submit locations for review.</p>
     <p class="card-text text-dark">Technologies: MongoDB, Express, React, and Node </p>
       </div>
-      <a href="https://github.com/rajonali/CRUD-Inventory-Mgmt-Tool" style={{padding:'10px', borderRadius:'0px', backgroundColor:'#d9534f'}} class="btn btn-danger">Go to Github Repo</a>
+      <a href="https://github.com/rajonali/applepayfinder" style={{padding:'10px', borderRadius:'0px', backgroundColor:'#d9534f'}} class="btn btn-danger">Go to Github Repo</a>
 
 </div>
 </div>
